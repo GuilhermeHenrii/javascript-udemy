b7e6aa953056889dc3cedb1d351370c317f2a878
Guilherme Henrique <guilhermesilva50239@gmail.com>
2022-12-06 17:13:26 -0300

    Update exercicios.js

--- aula8/exercicios.js
+++ aula8/exercicios.js
@@ -15,3 +15,8 @@ let anoAtual = 2022;
 const anoNascimento = anoAtual - idade;
 
 console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso}kg, tem ${alturaEmMetros} de altura e seu IMC é de ${imc}. ${nome} nasceu em ${anoNascimento}.`);
+
+const num1 = 10;
+const num2 = 20;
+const num3 = num2 + num1;
+console.log(num3);
