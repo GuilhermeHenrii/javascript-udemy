61e5674e53b76a0bde44320307eff06692acb6c8
Guilherme Henrique <guilhermesilva50239@gmail.com>
2022-12-14 16:45:43 -0300

    atualizando

---
+++ aula20.1/script/script.js
@@ -0,0 +1,28 @@
+function meuEscopo(){
+    const form = document.querySelector('.form');
+    const resp = document.querySelector('.resultado')
+    const pessoas = [];
+
+    function recebeEventoForm(evento){
+        evento.preventDefault();
+
+        const nome = form.querySelector('.nome');
+        const email = form.querySelector('.email');
+        const js = form.querySelector('.js')
+        const css = form.querySelector('.css')
+        const html = form.querySelector('.html')
+
+        pessoas.push({
+            nome:nome.value,
+            email:email.value,
+        })
+
+        console.log(pessoas);
+
+        resp.innerHTML = `Seu formulário foi enviado ${nome.value}!`;
+    }
+
+    form.addEventListener('submit', recebeEventoForm)
+}
+
+meuEscopo();
