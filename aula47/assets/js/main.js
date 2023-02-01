9887a8adcf2110a77cee12bad1bac64674c4208d
Guilherme Henrique <guilhermesilva50239@gmail.com>
2023-02-01 18:09:11 -0300

    exercicio finalizado

--- aula47/assets/js/main.js
+++ aula47/assets/js/main.js
@@ -1,12 +1,45 @@
-let text = document.querySelector('.container h1');
+function watch(){
+    function getTimeFromSecond(seconds){
+        const data = new Date(seconds * 1000)//multiplica por 1000 para ter o valor em segundos
+        return data.toLocaleTimeString('pt-BR', {
+            hour12: false,
+            timeZone: 'GMT'//time zone do br
+    
+        });
+    }
+    
+    const relogio = document.querySelector('.relogio');
+    let segundos = 0;
+    let timer;
+    
+    function startWatch(){
+        timer = setInterval(function(){
+            segundos++; 
+            relogio.innerHTML = getTimeFromSecond(segundos);      
+        }, 1000);
+    }
+    
+    document.addEventListener('click', function(events){
+        const elements = events.target;//vai retornar, na variavel elements, qual elemento do documento foi clicado
 
-function getHours(){
-    const hora = new Date();
-    return hora.toLocaleTimeString('pt-BR');
-}
-const timer = function(){
+        if(elements.classList.contains('iniciar')){
+            relogio.classList.remove('pausado');
+            clearInterval(timer);
+            startWatch();
+        }
+    
+        if(elements.classList.contains('pausar')){
+            clearInterval(timer);
+            relogio.classList.add('pausado');
+        }
+
+        if(elements.classList.contains('zerar')){
+            clearInterval(timer);
+            relogio.innerHTML = '00:00:00';
+            relogio.classList.remove('pausado');
+            segundos = 0; 
+        }
+    })
     
 }
-
-
-text.innerHTML = `${getHours()}`;
+watch();
