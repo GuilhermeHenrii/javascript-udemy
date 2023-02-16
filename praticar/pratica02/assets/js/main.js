fd371843bf61f1aafcd441f666c6f9a813106bfd
Guilherme Henrique <guilhermesilva50239@gmail.com>
2023-02-16 18:33:33 -0300

    css e criação de botoes

--- praticar/pratica02/assets/js/main.js
+++ praticar/pratica02/assets/js/main.js
@@ -24,9 +24,20 @@ function createDiv(className){
 document.addEventListener('DOMContentLoaded' , function(){
     const divCalc = container(createDiv('div-calc'));
     const divBtn = createDiv('div-btn');
+    const divNum = createDiv('div-num');
     divCalc.appendChild(divBtn);
+    divCalc.appendChild(divNum);
     divBtn.appendChild(createButton('btn-sum', '+'));
     divBtn.appendChild(createButton('btn-sub', '-'));
     divBtn.appendChild(createButton('btn-mult', '*'));
     divBtn.appendChild(createButton('btn-divi', '÷'));
+    divNum.appendChild(createButton('btn-num1', '1'));
+    divNum.appendChild(createButton('btn-num2', '2'));
+    divNum.appendChild(createButton('btn-num3', '3'));
+    divNum.appendChild(createButton('btn-num4', '4'));
+    divNum.appendChild(createButton('btn-num5', '5'));
+    divNum.appendChild(createButton('btn-num6', '6'));
+    divNum.appendChild(createButton('btn-num7', '7'));
+    divNum.appendChild(createButton('btn-num8', '8'));
+    divNum.appendChild(createButton('btn-num9', '9'));
 })
