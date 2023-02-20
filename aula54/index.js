d3329adc0a875749a180ebc19935d5d78641c437
Guilherme Henrique <guilhermesilva50239@gmail.com>
2023-02-20 09:38:14 -0300

    Update index.js

--- aula54/index.js
+++ aula54/index.js
@@ -40,7 +40,7 @@ function f1callback(){//função que foi passada como parametro sendo executada
     f2(f2callback);//f2 sendo executado com um parametro que tbm sera executado como uma função
 }
 
-function f2callback(){//e assim sucessivamente, até executarmos a ordem que desejamos de execução das fubçoes 
+function f2callback(){//e assim sucessivamente, até executarmos a ordem que desejamos de execução das funçoes 
     f3(f3callback);
 }
 
