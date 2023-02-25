76bd2cd87ca9a4600cad2781aa65a802e4d8a47a
Guilherme Henrique <guilhermesilva50239@gmail.com>
2023-02-25 11:50:16 -0300

    factory function

---
+++ aula56/index.js
@@ -0,0 +1,11 @@
+//Factory functions
+
+const obj = {
+    nome: 'guilherme',
+    sobrenome: 'henrique',
+    falar: function(assunto){
+        return `${nome} está ${assunto}`;
+    }
+}
+
+const p1 = 
