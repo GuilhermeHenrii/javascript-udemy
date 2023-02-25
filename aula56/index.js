8f150ab5d3ed36b536ecd58b767896666fb1e2f9
Guilherme Henrique <guilhermesilva50239@gmail.com>
2023-02-25 16:47:58 -0300

    Update index.js

--- aula56/index.js
+++ aula56/index.js
@@ -1,22 +1,34 @@
 //Factory functions
-function criaPessoa(nome, sobrenome, p, a){
+function criaPessoa(nome, sobrenome, assunto, p, a){//Função fabrica faz o uso do this
     return{
         nome,
         sobrenome,
-        fala(assunto){
-            return `${this.nome} esta falando sobre ${assunto}`;
+        assunto,
+
+        //getter
+        get nomeCompleto(){
+            
         },
-        peso = p;
-        altura: a
-        imc(peso, altura){
-            const calcImc = peso/(altura*altura);
+        
+        //setter
+        set nomeCompleto(){
+
+        }
+
+        fala(){
+            return `${this.nome} esta falando sobre ${this.assunto}`;
+        },
+        peso : p,
+        altura: a,
+        get imc(){
+            const calcImc = this.peso/(this.altura*this.altura);
             return calcImc.toFixed(2);
         }
     }
 }
 
-const pessoa1 = criaPessoa('Guilherme', 'Henrique');
-const pessoa2 = criaPessoa('Emily', 'Silva');//Podemos ver que aqui apenas precisamos informar os parametros e o objeto é criado, caracterizando uma função construtora
+const pessoa1 = criaPessoa('Guilherme', 'Henrique', 'JavaScript', 80, 1.70);
+const pessoa2 = criaPessoa('Emily', 'Silva','CSS', 60, 1.62);//Podemos ver que aqui apenas precisamos informar os parametros e o objeto é criado, caracterizando uma função fábrica
 
-console.log(pessoa1.fala('programação'));
-console.log(pessoa1.imc(60, 1.70));
+console.log(pessoa2.fala('Programação'));
+console.log(pessoa2.imc);
