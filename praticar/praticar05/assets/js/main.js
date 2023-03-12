7f51600c0d1e8ebedd530f71df033fa99b2ba15b
Guilherme Henrique <guilhermesilva50239@gmail.com>
2023-03-12 10:47:54 -0300

    praticando factory functions

--- praticar/praticar05/assets/js/main.js
+++ praticar/praticar05/assets/js/main.js
@@ -1,21 +1,45 @@
-const masc = document.querySelector('#genM');
-const feme = document.querySelector('#genF');
-const main = document.querySelector(".main");
-const date = new Date();
-const inputDt = document.querySelector('#dateId');
-const dtBirth = date.value - inputDt.value;
+// const masc = document.querySelector('#genM');
+// const feme = document.querySelector('#genF');
+// const main = document.querySelector(".main");
+// const date = new Date().getFullYear();
+// const yearOfBirth = document.querySelector('.yearOfB');
+// let calcIdade = date - yearOfBirth.value;
 
+// main.addEventListener('click', function(e){
+//     const el = e.target;
+//     if(el.classList.contains('btn')){
+//        if(masc.checked && date - yearOfBirth.value <= 17){
+//         alert('homem');
+//         alert('criança');
+//        }else if(feme.checked){
+//         alert('mulher');
+//        }else{
+//         alert("preencha todos os campos")
+//        }
+//     }
+// });
 
-main.addEventListener('click', function(e){
-    const el = e.target;
-    if(el.classList.contains('btn')){
-       if(masc.checked){
-        alert('homem');
-        alert(dtBirth);
-       }else if(feme.checked){
-        alert('mulher');
-       }else{
-        alert("preencha todos os campos")
-       }
+function creatPerson(){
+    return {
+        masc: document.querySelector('.genM'),
+        fem: document.querySelector('genF'),
+        yearOfBirth: document.querySelector('.yearOfB'),
+
+        
+        getAge(){
+            document.addEventListener('click', function(e){
+                const el = e.target;
+
+                if(el.classList.contains('btn')){
+                    const date = new Date().getFullYear;
+                    const age = this.yearOfBirth - date;
+                    alert(this.yearOfBirth);
+                }
+            })
+        },
+
     }
-});
+}
+
+const person = creatPerson();
+person.getAge();
