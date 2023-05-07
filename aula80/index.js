236c119333cd0c2ae376574d1b5e6fc30c5a460e
Guilherme Henrique <guilhermesilva50239@gmail.com>
2023-05-06 22:22:46 -0300

    class javascript

--- aula80/index.js
+++ aula80/index.js
@@ -1,6 +1,7 @@
 //Classes em JS
+//Jeito mais moderno de definir classes, tem alguns vantagens em relação as funcoes construtoras
 class Pessoa {
-    constructor(nome, sobrenome){//construtor
+    constructor(nome, sobrenome){//Construtor é chamado automaticamento quando a palavra new é usada 
         this.nome = nome;
         this.sobrenome = sobrenome;
     }
@@ -18,10 +19,24 @@ class Pessoa {
     }
 }
 
+//Iremos observar que todos os métodos acima estará no prototype da classe Pessoa e irá herdar para todas as intancias, sem fazer nenhuma operação no prototype se quer. O JS se encarregará de fazer isso sozinho.
+//Apenas uma instancia em memoria será criada
 const p1 = new Pessoa('Guilherme', 'Henrique');
+const p2 = new Pessoa('Pedro', 'Lucas');
+const p3 = new Pessoa('Jose', 'Lopes');
+const p4 = new Pessoa('Ronaldo', 'Silva');
 console.log(p1);
+console.log(p2);
+console.log(p3);
+console.log(p4);
 
-
+const [...p] = [
+    new Pessoa('lucas', 'Henrique'),
+    new Pessoa('Pedro', 'Lucas'),
+    new Pessoa('Jose'), 
+    new Pessoa('Ronaldo', 'Silva'),
+];
+console.log(p[0]);
 /********************************************************************************************************************************************************************************************/
 
 
@@ -29,6 +44,8 @@ console.log(p1);
 function Pessoa2(nome, sobrenome){
     this.nome = nome;
     this.sobrenome = sobrenome;
+
+    
 }
 
 Pessoa2.prototype.falar = function(){
