cf9b40afe565ac072a07aeeeccb8bfa5ccee85d4
Guilherme Henrique <guilhermesilva50239@gmail.com>
2023-05-29 16:54:30 -0300

    Update main.js

--- aula85/assets/js/main.js
+++ aula85/assets/js/main.js
@@ -16,7 +16,7 @@ class ValidaFormulario {
     const camposValidos = this.camposSaoValidos();
     const senhasValidas = this.senhasSaoValidas();
 
-    if(camposValidos && senhasValidas) {
+    if (camposValidos && senhasValidas) {
       alert('Formulário enviado.');
       this.formulario.submit();
     }
@@ -28,13 +28,13 @@ class ValidaFormulario {
     const senha = this.formulario.querySelector('.senha');
     const repetirSenha = this.formulario.querySelector('.repetir-senha');
 
-    if(senha.value !== repetirSenha.value) {
+    if (senha.value !== repetirSenha.value) {
       valid = false;
       this.criaErro(senha, 'Campos senha e repetir senha precisar ser iguais.');
       this.criaErro(repetirSenha, 'Campos senha e repetir senha precisar ser iguais.');
     }
 
-    if(senha.value.length < 6 || senha.value.length > 12) {
+    if (senha.value.length < 6 || senha.value.length > 12) {
       valid = false;
       this.criaErro(senha, 'Senha precisa estar entre 6 e 12 caracteres.');
     }
@@ -45,24 +45,24 @@ class ValidaFormulario {
   camposSaoValidos() {
     let valid = true;
 
-    for(let errorText of this.formulario.querySelectorAll('.error-text')) {
+    for (let errorText of this.formulario.querySelectorAll('.error-text')) {
       errorText.remove();
-    }
+    };
 
-    for(let campo of this.formulario.querySelectorAll('.validar')) {
+    for (let campo of this.formulario.querySelectorAll('.validar')) {
       const label = campo.previousElementSibling.innerText;
 
-      if(!campo.value) {
+      if (!campo.value) {
         this.criaErro(campo, `Campo "${label}" não pode estar em branco.`);
         valid = false;
       }
 
-      if(campo.classList.contains('cpf')) {
-        if(!this.validaCPF(campo)) valid = false;
+      if (campo.classList.contains('cpf')) {
+        if (!this.validaCPF(campo)) valid = false;
       }
 
-      if(campo.classList.contains('usuario')) {
-        if(!this.validaUsuario(campo)) valid = false;
+      if (campo.classList.contains('usuario')) {
+        if (!this.validaUsuario(campo)) valid = false;
       }
 
     }
@@ -74,12 +74,12 @@ class ValidaFormulario {
     const usuario = campo.value;
     let valid = true;
 
-    if(usuario.length < 3 || usuario.length > 12) {
+    if (usuario.length < 3 || usuario.length > 12) {
       this.criaErro(campo, 'Usuário precisa ter entre 3 e 12 caracteres.');
       valid = false;
     }
 
-    if(!usuario.match(/^[a-zA-Z0-9]+$/g)) {
+    if (!usuario.match(/^[a-zA-Z0-9]+$/g)) {
       this.criaErro(campo, 'Nome de usuário precisar conter apenas letras e/ou números.');
       valid = false;
     }
@@ -90,7 +90,7 @@ class ValidaFormulario {
   validaCPF(campo) {
     const cpf = new ValidaCPF(campo.value);
 
-    if(!cpf.valida()) {
+    if (!cpf.valida()) {
       this.criaErro(campo, 'CPF inválido.');
       return false;
     }
