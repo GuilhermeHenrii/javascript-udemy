a9e6fe2f4555679b2232d0909b392b86640f1baa
Guilherme Henrique <guilhermesilva50239@gmail.com>
2023-06-01 19:57:48 -0300

    babel

---
+++ aula92/main.js
@@ -0,0 +1,12 @@
+class Pessoa{
+    constructor(nome, sobrenome){
+        this.nome = nome,
+        this.sobrenome = sobrenome
+    }
+
+    falaoi(){
+        console.log('Oi');
+    }
+}
+
+//Usando o babel, podemos converter o código js para uma versao mais aintiga, versão essa aceita em todos os navegadores. 
