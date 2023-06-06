8cd95427795941d2dc25613a59980620d5891407
Guilherme Henrique <guilhermesilva50239@gmail.com>
2023-06-06 16:11:24 -0300

    webpack

---
+++ aula93/src/index.js
@@ -0,0 +1,7 @@
+//1
+//2
+//3
+console.log('oiiiiiiiiiiiiiiiiiiiiiiii');
+
+//O -w no package-json significa whatch, ou seja, o webpack fica 'assistindo' o codigo e todas as alterações são feitas em tempo de execução.
+//Assim como no babel, que quando o executamos ele fica rodando, ate o momento em que quisermos parara-lo.
