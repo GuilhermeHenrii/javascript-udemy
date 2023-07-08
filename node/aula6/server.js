b178657501a6b52242d5d44768670483bf239232
Guilherme Henrique <guilhermesilva50239@gmail.com>
2023-07-08 12:54:45 -0300

    express

---
+++ node/aula6/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) =>{
+    res.send(('Hello Word'));
+});
+
+
+app.get('/contato', (req, res) => {
+    res.send('Obrigado por entrar em contato');
+});
+
+
+app.listen(3000, () =>{
+    console.log('Acessar http://localhost:3000')
+    console.log('Servidor executando na porta 3000');
+});
