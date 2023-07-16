25dd21a0b9699eacb9f5867d5e4474475e9b50f3
Guilherme Henrique <guilhermesilva50239@gmail.com>
2023-07-16 13:38:00 -0300

    models

--- node/aula14/src/controllers/homeControllers.js
+++ node/aula14/src/controllers/homeControllers.js
@@ -1,3 +1,14 @@
+const HomeModel = require('../models/HomeModel');
+
+//criando um modelo na base de dados
+HomeModel.create({
+    titulo: 'Um titulo de testes qualquer',
+    descricao: 'Uma descricao de destes'
+})
+    .then((dados) => console.log(dados))
+    .catch((e) => console.log(e));
+
+
 exports.homePage = (req, res, next) => {
     console.log(`Primeiero midlleware req.session.sobrenome: ${req.session.nome}`);
     res.render('index');//aqui no controlador estou chamando a view
